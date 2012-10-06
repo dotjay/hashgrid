a95a041f78018bc67b848dfa94f7d820b1c7f4f8
Jon Gibbins <dotjay@gmail.com>
2012-10-06 16:10:47 +0100

    Link to the latest hashgrid.js file. Update some links.

--- hashgrid.js
+++ hashgrid.js
@@ -35,19 +35,16 @@
  * // The basic #grid setup looks like this
  * var grid = new hashgrid();
  *
- * // Or you can set a custom id for your grid, e.g. #mygrid
- * var grid = new hashgrid("mygrid");
- *
  * // But there are a whole bunch of additional options you can set
  * var grid = new hashgrid({
- *     id: 'mygrid',            // id for the grid container
+ *     id: 'mygrid',            // set a custom id for the grid container
  *     modifierKey: 'alt',      // optional 'ctrl', 'alt' or 'shift'
  *     showGridKey: 's',        // key to show the grid
  *     holdGridKey: 'enter',    // key to hold the grid in place
  *     foregroundKey: 'f',      // key to toggle foreground/background
  *     jumpGridsKey: 'd',       // key to cycle through the grid classes
  *     numberOfGrids: 2,        // number of grid classes used
- *     classPrefix: 'class',    // prefix for the grid classes
+ *     classPrefix: 'myclass',  // prefix for the grid classes
  *     cookiePrefix: 'mygrid'   // prefix for the cookie name
  * });
  */
